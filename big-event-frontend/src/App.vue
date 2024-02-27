8db59bbd9f61c00baafbc27314e6fcbab7d06e5a
boyo <zhijunc664@gmail.com>
2024-02-27 16:54:50 +0800

    frontend

---
+++ big-event-frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import LoginVue from "@/views/Login.vue";
+  import  Layout from "@/views/Layout.vue";
+</script>
+
+<template>
+  <!-- <LoginVue/> -->
+  <Layout/>
+</template>
+
+<style scoped>
+  
+</style>
